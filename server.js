e2efa81499ebd384c2ce45d46a2c3dfec4caad25
Tatjana <huesch.tatjana@gmail.com>
2022-09-26 14:50:48 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-xss-demo'));
+app.use(express.static(__dirname + '/dist/xss-benchmark-spa'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/angular-xss-demo/index.html'));});
+    '/dist/xss-benchmark-spa/index.html'));});
 app.listen(process.env.PORT || 8080);
